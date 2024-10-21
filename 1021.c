09558e32099cc34814d93be2ee2478eaacfd2740
codeKing6412 <3572163214@qq.com>
2024-10-21 23:31:07 +0800

    Add files via upload

---
+++ 1021.c
@@ -0,0 +1,62 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+//int Add(int x, int y)
+//{
+//	return x + y*3;
+//}
+//int main()
+//{
+//	int n1 = 0;
+//	int n2 = 0;
+//	scanf("%d %d", &n1,&n2);
+//	int sum = Add(n1, n2);
+//	printf("%d\n", sum);
+//		return 0;
+//
+//
+//}
+//int num = 10;
+//int main()
+//{
+//	int num = 1;
+//	printf("num=%d\n", num);
+//	return 0;
+//}
+//int main()
+//{
+//	printf("     **\n");
+//	printf("     **\n");
+//	printf("************\n");
+//	printf("************\n");
+//	printf("     * *\n");
+//	printf("     * *\n");
+//	return 0;
+//}
+// ////作业：小飞机打印
+//int main()
+//{
+//	printf("Name    Age    Gender\n");
+//	printf("---------------------\n");
+//	printf("Jack    18    man    \n");
+//	return 0;
+//}
+//	//作业：名单打印
+//int main()
+//{
+//	printf("I lost my cellphone!");
+//	return 0;
+//}
+////作业：手机丢失打印
+int Add(int x,int y)
+{
+	return (-8 + 22) * x - 10 + y / 2;
+}
+int main()
+{
+	int n1 = 0;
+	int n2 = 0;
+	scanf("%d %d", &n1, &n2);
+	int sum = Add(n1, n2);
+	printf("%d\n", sum);
+		return 0;
+}
