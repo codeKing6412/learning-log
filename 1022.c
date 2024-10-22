7aec36bb4736bd471cc52cec1eed7c347ed6bb71
codeKing6412 <3572163214@qq.com>
2024-10-22 23:31:12 +0800

    Add files via upload

---
+++ 1022.c
@@ -0,0 +1,38 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+//int F(int x, int y)
+//{
+//	if (x < 0)
+//		return(y = 1);
+//	if (x > 0)
+//		return(y = -1);
+//	else
+//		return(y = 0);
+//
+//}
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	scanf("%d", &a);
+//	int r = F(a,b);
+//	printf("%d", r);
+//	return 0;
+//}作业：输出函数
+int main()
+{
+	int a = 0;
+	char b = "%&)";
+	long c = 0;
+	long long d = 0;
+	float e = 0;
+	double f = 0;
+	printf("%d\n", sizeof a);
+	printf("%d\n", sizeof b);
+	printf("%d\n", sizeof c);
+	printf("%d\n", sizeof d);
+	printf("%d\n", sizeof e);
+	printf("%d\n", sizeof f);
+		return 0;
+
+}
