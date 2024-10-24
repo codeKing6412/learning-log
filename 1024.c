cadb98a824618237a37dda0b62aacd3aa5514c21
codeKing6412 <3572163214@qq.com>
2024-10-24 23:46:55 +0800

    Add files via upload

---
+++ 1024.c
@@ -0,0 +1,42 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+//int main()
+//{
+//	int age = 16;
+//	if (age < 18)
+//		printf("玩原神");
+//	else
+//		printf("玩人生");
+//	return 0;
+//}
+//int main()
+//{
+//	int age = 0;
+//	scanf("%d", &age);
+//	if (age < 18)
+//		printf("不许玩原神\n");
+//	else if (age >= 18 && age < 22)
+//		printf("大学生！正是玩原神的时候！\n");
+//	else if (age >= 22 && age <= 60)
+//		printf("还玩？赶紧挣钱去！\n");
+//	else 
+//		printf("玩你的吧，原神——启动！\n");
+//	return 0;
+//
+//
+//}
+//int main()
+//{
+//	char a = 259;
+//	printf("%d\n", a);
+//	int b = 9999999;
+//		printf("%d\n", b);
+//	return 0;
+//}
+//int main()
+//{
+//	int a = 6;
+//	if (5 == a);//if(a == 5)
+//	printf("不会出现找不到错误的问题");
+//		return 0;
+//}
