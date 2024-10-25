7cb9e402b15b6485e58cea160fc69dbab2784362
codeKing6412 <3572163214@qq.com>
2024-10-25 23:23:03 +0800

    Add files via upload

---
+++ 1025.c
@@ -0,0 +1,152 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+//int main()
+//{
+//	printf("请输入一个数字");
+//	//判断一个数是否为偶数
+//	int num = 0;
+//	scanf("%d", &num);
+//	//判断过程
+//	if (num % 2 == 2)
+//		printf("这个数是偶数");
+//	else if (num % 2 == 0)
+//	printf("这个数是偶数");
+//	else printf("这个数为奇数");
+//	return 0;
+//}
+////另解
+//int main()
+//{
+//	printf("请输入一个数字");
+//	//判断一个数是否为偶数
+//	int num = 0;
+//	scanf("%d", &num);
+//	//判断过程
+//	if ("num % 2 = 1")
+//		printf("这个数是奇数");
+//	else
+//		printf("这个数是偶数");
+//	return 0;
+//
+//}
+//int main()
+//{
+//	//输出1~100间的奇数
+//	int a = 0;
+//	while (a <= 100)
+//	{
+//		if (a % 2 == 1)
+//		{
+//			printf("%d\n", a);
+//		}
+//		a++;
+//	}
+//	return 0;
+//}
+//另解
+//int main()
+//{
+//	int a = 1;
+//		while (a <= 100)
+//		{
+//			if (a % 2 == 1)
+//				printf("%d\n", a);
+//			a += 2;
+//	}
+//	return 0;
+//}
+//int main()
+//{
+//	int a = 1;
+//	scanf("%d", &a);
+//	if (1 == a)
+//	{
+//		printf("星期一");
+//
+//	}
+//	if (2 == a)
+//	{
+//		printf("星期二");
+//	
+//	}
+//	if (3 == a)
+//	{
+//		printf("星期三");
+//
+//	}
+//	if (4 == a)
+//	{
+//		printf("星期四");
+//	}
+//	if (5 == a)
+//	{
+//		printf("星期五");
+//	}
+//	if (6 == a)
+//	{
+//		printf("星期六");
+//	}
+//	if (7 == a)
+//	{
+//		printf("星期日");
+//	}
+//	return 0;
+//}
+//int main()
+//{
+//	int day = 1;
+//	scanf("%d", &day);
+//	switch (day)
+//	{
+//	case 1:
+//		printf("星期一");
+//		break;
+//	case 2:
+//		printf("星期二");
+//		break;
+//	case 3:
+//		printf("星期三");
+//		break;
+//	case 4:
+//		printf("星期四");
+//		break;
+//	case 5:
+//		printf("星期五");
+//		break;
+//	case 6:
+//		printf("星期六");
+//		break;
+//	case 7:
+//		printf("星期日");
+//		break;
+//	default:
+//		printf("输入错误");
+//		break;
+//	}
+//	return 0;
+//}
+//int main()
+//{
+//	int a = 0;
+//	scanf("%d", &a);
+//	switch (a)
+//	{
+//		//case关键字相当于切入符
+//	case 1:
+//	case 2:
+//	case 3:
+//	case 4:
+//	case 5:
+//		printf("weekday");
+//		break;
+//	case 6:
+//	case 7:
+//		printf("weekend");
+//		break;
+//	default:
+//		printf("you have input a wrong number");
+//      break;
+//	}
+//	return 0;
+//}
+
