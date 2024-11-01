91b91bb4fedd318cf5281981b0a36910db9a7809
codeKing6412 <3572163214@qq.com>
2024-11-01 10:16:47 +0800

    Add files via upload

---
+++ 1031.c
@@ -0,0 +1,37 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+//int main()
+//{
+//    char input[20] = { 0 };
+//    system("shutdown -s -t 60");
+//    printf("你的电脑将在60秒内关机，如果输入：“我是蠢物”，即可取消关机。");
+//again:
+//    scanf("%s", input);
+//    if (strcmp(input, "我是蠢物") == 0)
+//    {
+//        system("shutdown -a");
+//    }
+//    else
+//    {
+//        printf("蠢物！这点亏都不愿意吃！");
+//        goto again;
+//    }
+//    return 0;
+//}
+//int main()
+//{
+//	char arr1[20] = { 0 };
+//	char arr2[] = "hello world";
+//	strcpy(arr1,arr2);
+//	printf("%s\n", arr1);
+//	memset(arr1, '*', 5);
+//	printf("%s\n", arr1);
+//	return 0;
+//}
+//int a = 0;
+//int b = 0;
+//a = 30;
+//char*p = &a;
+//*p = 30;
