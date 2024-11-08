503313f9ffb767aa8a057b1d17e33c0fc3e9355f
codeKing6412 <3572163214@qq.com>
2024-11-08 08:58:36 +0800

    Add files via upload

---
+++ 241107.c
@@ -0,0 +1,39 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+//void print_table(int x)
+//{
+//	int i = 0;
+//	for (i = 1; i <= x; i++)
+//	{
+//		int j = 0;
+//		for (j = 1; j <= i; j++)
+//		printf("%d*%d=%2d ", i, j, i * j);
+//		printf("\n");
+//	}
+//}
+//int main()
+//{
+//	int n = 0;
+//	scanf("%d", &n);
+//	print_table(n);
+//
+//	return 0;
+//}
+// 打印99乘法表
+
+#include<stdlib.h>
+#include<windows.h>
+int main()
+{
+    system("color e");
+    printf("\t\t\t  亲爱滴妈妈\n\n");
+    printf("\t\t\t               \n");
+    printf("\t\t\t       |       \n");
+    printf("\t\t\t  *  ooooo  *  \n");
+    printf("\t\t\t  |  |   |  |  \n");
+    printf("\t\t\tooooooooooooooo\n");
+    printf("\t\t\t| 生 日 快 乐 |\n");
+    printf("\t\t\t|_____________|\n\n");
+    printf("\n\t\t\tHAPPY BIRTHDAY！\n\n");
+    return 0;
+}
