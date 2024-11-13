13d197824b3e3318716eeed736a5e313519121e1
codeKing6412 <3572163214@qq.com>
2024-11-13 12:27:40 +0800

    Update game(241112).h

--- game(241109).h
+++ game(241109).h
@@ -1,7 +1,15 @@
 #pragma once
 //游戏代码的声明（函数声明，符号定义）
 #include<stdio.h>
-#define ROW 10
-#define COL 10
+#include<stdlib.h>
+#include<time.h>
+#define ROW 3
+#define COL 3
 //初始化棋盘
 void InitBoard(char board[ROW][COL], int row, int col);
+//打印棋盘
+void DisplayBoard(char board[ROW][COL], int row, int col);
+//玩家下棋
+void PlayerMove(char board[ROW][COL], int row, int col);
+//电脑下棋
+void ComputerMove(char board[ROW][COL], int row, int col);
