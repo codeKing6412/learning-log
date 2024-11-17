0abc0ddaa3343895b818b9b9abdf7df2918c6f2f
codeKing6412 <3572163214@qq.com>
2024-11-17 22:31:33 +0800

    Add files via upload

---
+++ study.c
@@ -0,0 +1,52 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+////1.字面常量
+//a = 30;
+//enum Color
+//{
+//	BLOND,
+//	GOLD,
+//	RED
+//};
+//const b = 50;
+//#define MIN 100;
+//#define STR "A+"
+//	; int main()
+//{
+//	enum Color c = GOLD;
+//	int d = MIN
+//		printf("你的评级要达到%s\n", STR);
+//	    printf("你最少考到%d\n", d);
+//	return 0;
+//}
+//double main()
+//{
+//	char arr1 = "abcdef";
+//	char arr2 = { 'a' };
+//	printf("%s\n", arr1);
+//	printf("%s\n", arr2);
+//	
+//	return 0;
+//}
+//int main()
+//{
+//	printf("\a");
+//	return 0;
+//
+//}
+//int Add(int x, int y)
+//{
+//	return x + y*3;
+//}
+//int main()
+//{
+//	int n1 = 0;
+//	int n2 = 0;
+//	scanf("%d %d", &n1,&n2);
+//	int sum = Add(n1, n2);
+//	printf("%d\n", sum);
+//		return 0;
+//
+//
+//}
